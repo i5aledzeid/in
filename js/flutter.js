82e61012a8737fea1efee260debf37dce47d181d
Khaled Zeid <107222642+i5aledzeid@users.noreply.github.com>
2022-08-01 15:12:46 +0200

    Add files via upload

--- js/flutter.js
+++ js/flutter.js
@@ -1,11 +1,11 @@
-alert("Welcome to flutter, script");
+alert("Welcome to flutter, script v2");
 let starts = document.getElementById('starts');
 let moon = document.getElementById('moon');
 let mountains3 = document.getElementById('mountains3');
 let mountains4 = document.getElementById('mountains4');
 let river = document.getElementById('river');
 let boat = document.getElementById('boat');
-let flutter_title = document.querySelector('.flutter-title');
+let title = document.querySelector('.title');
 
 window.onscroll = function () {
     let value = scrollY;
@@ -16,15 +16,15 @@ window.onscroll = function () {
     river.style.top = value + 'px';
     boat.style.top = value + 'px';
     boat.style.left = value + 3 + 'px';
-    flutter_title.style.fontSize = value + 'px';
+    title.style.fontSize = value + 'px';
     if (scrollY >= 67) {
-        flutter_title.style.fontSize = 67 + 'px';
-        flutter_title.style.position = 'fixed';
+        title.style.fontSize = 67 + 'px';
+        title.style.position = 'fixed';
         if (scrollY >= 478) {
-            flutter_title.style.display = 'none';
+            title.style.display = 'none';
         }
         else {
-            flutter_title.style.display = 'block';
+            title.style.display = 'block';
         }
         if (scrollY >= 127) {
             document.querySelector('.main').style.background = 'linear-gradient(#376281, #10001f)';
